fe94c67d2242d6975ce0ce862772de382104e084
Divyanshu-Tripathi <85430452+Divyanshu-Tripathi@users.noreply.github.com>
2021-06-06 12:40:59 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note()
+{
+  return <header>
+    
+    <div className="note">
+    <h1>Javascript and react.js</h1>
+    <p>This was an amazing bootcamp.My name is Divyanshu tripathi Btech First year With IT</p></div>
+        </header>
+}
+export default Note;
